9166f88fb29905cf1fc7dfb3dc7435e090b96598
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-07-08 14:23:35 +0300

    Debugging Exercise

---
+++ Day 1/Exercise2.py
@@ -0,0 +1,14 @@
+""" 
+print(Day 1 - String Manipulation)
+print("String Concatenation is Done with the "+" sign.")
+  print('e.g print("Hello " + "world")')
+  print(("New Lines can be created with a backslash and n."))
+
+
+ DEBUG THIS CODE
+"""
+
+print('Day 1 - String Manipulation')
+print("String Concatenation is Done with the '+' sign.")
+print('e.g. print("Hello " + "world")')
+print("New Lines can be created with a backslash and n.")
