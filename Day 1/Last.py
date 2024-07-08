821787a383efde001c98258e0b82c69519f20577
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-07-08 15:50:00 +0300

    Band Name Generator

---
+++ Day 1/Last.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Generator")
+name_city = input('What is tne name of the city you grew up in? \n')
+pet_name = input('What is Your pet Name ?\n ')
+print("Your band name couid be "+ name_city + " "+ pet_name)
