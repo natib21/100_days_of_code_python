ff4a78aadec476b3814e6d918abe0aa8123b60cc
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-07-10 17:06:59 +0300

    coditional operators if/else

---
+++ Day 3/main.py
@@ -0,0 +1,7 @@
+print("Welcome to The roller coaster !")
+height = int(input("What is you height ? "))
+
+if height > 120 :
+    print("You Can ride The Roller Coaster")
+else :
+    print("Sorry, you have to grow taller before you can ride ")    
